aee4a08e089185cf21e5bf01a6ab091a93ee1a26
greenspook001 <160692620+greenspook001@users.noreply.github.com>
2024-05-01 16:23:02 +1200

    Create problem3.py

---
+++ problem3.py
@@ -0,0 +1,65 @@
+
+name = input("Tell us your name - ")
+ 
+input("And now you can begin")
+input("...")
+
+print("Morning Captain '' + name + ". It's good to see your well rested, but we need to talk")
+
+input("...")
+
+print('' Last time we were in Gorland to get some food for the journey. I heard these fellow pirates speaking of some riches and Gold and Diamonds on a island so vividly, so much that it got my hopes us.``)
+
+input("...")
+
+print("But then, they said it didn't exist. They saw it as being some type of myth, much less a fairytale, told many times. So you tried anyways to find these goods.And now Captain " + name + ", we need your help...")
+input("")
+print("We need you to find the treasure!")
+
+input("...")
+input("...")
+input("...")
+
+print("After you woke up, you stood wondering where on earth you were, and so you spoke to your crew")
+
+input("They told you that you were in the middle of the atlantic sea, and that the legend said that it was somewhere here.")
+
+input("...")
+
+print("You go to the front of your boat and look around. You see 3 paths, west, north, and east. And so you think...")
+
+input("Do you really want to do this?")
+
+paths = input("Which way shall you go? - ")
+
+if paths == "west":
+    print("You sail west, where you find a desert island. You recognize it to be Windywisks, a place where there was nothing for miles. and so you travelled to the land...")
+    input("...")
+    print("and so after you sailed ahead, suddenly your boat began to get hotter and hotter by the minute. You ask your fellow crew if they knew anything of this. And they all respond no.")
+    input("")
+    print("But before it was too late your boats breaks in half and your crew get swinged into the ocean water, where they burn alive by the flaming hot sea near the island, and the rest burn as you start to get near to the island.")
+    
+    input("And so you die, from too much heat, and you too boil alive")
+    input("You died, care to try again?")
+    
+elif paths == "east":
+    print("You choose to sail east, to the Sea of Waves")
+    input(" 5 days later, one of your crew members say 'Strange captain, the sea here ain't so rough like we used to know, why ain't it full of waves and storms?.")
+    input("You reply with a nug, as if you couldn't tell why")
+    input("Then out of nowhere a giant sea kraken emerges from the depths of the ocean and raps his tenticals all over your ship, cracking and snapping the wood you once knew to be your ship.")
+    input("You and the crew decide to jump off the ship, assuming  you would be found and so, you and your crew get eaten by sirens. Magical beasts of voices so beautiful and so deadly, luring you into there deadly vocal trap.")
+    input("And so you die, eaten by those beautiful yet, dreadful creatures.")
+    input("...")
+    
+    input("You died, care to try again?")
+    
+elif paths == "north":
+    print("You sail north, in hopes to find the lost treasure. So you take your ship and crew and sail for 2 days straight.")
+    input("You - in such pain and patience - find an island. But not the typical island...")
+    input("Island Thermo, a dark and cursed Island with powerful beasts roaming everywhere you look. Your boat got closer and you decided to try and slow it down. But it couldn't, it wouldn't! It just kept going, and going, and never stoped.")
+    input("You turned to slumber and slept for a long time...")
+    
+    Input("When")
+    
+else:
+    print("you still need to choose!")
